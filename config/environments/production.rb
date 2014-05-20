d9fb22aa7d2e9b6401e50610ee653c1bde4f8eda
CJ Avilla <cjavilla@gmail.com>
2014-05-19 21:22:01 -0700

    playing in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,9 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
+  config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')
+  config.assets.precompile += %w( .svg .eot .woff .ttf )
 
   # Generate digests for assets URLs.
   config.assets.digest = true
