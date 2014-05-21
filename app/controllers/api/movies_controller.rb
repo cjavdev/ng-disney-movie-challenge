29b168a195ac8e960a51669beca248bf937e965c
CJ Avilla <cjavilla@gmail.com>
2014-05-20 22:22:43 -0700

    ordering movie released date

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -2,7 +2,7 @@ class Api::MoviesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @movies = Movie.all
+    @movies = Movie.order(released_at: :asc)
     @ratings = {}
     current_user.ratings.map { |r| @ratings[r.movie_id] = r }
 
