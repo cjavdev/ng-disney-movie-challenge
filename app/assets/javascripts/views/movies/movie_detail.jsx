1d6e2a520f3cfd0c4426a047078cc14df6c2f9eb
CJ Avilla <cjavilla@gmail.com>
2014-05-21 18:26:04 -0700

    adding movie detail

--- app/assets/javascripts/views/movies/movie_detail.jsx
+++ app/assets/javascripts/views/movies/movie_detail.jsx
@@ -5,7 +5,16 @@
 var MovieDetail = React.createClass({
   render: function () {
     return (
-      <div> movie detail stuff </div>
+      <div>
+        <h2>{this.props.movie.get('name')}</h2>
+        <p><strong>Released:</strong> {moment(this.props.movie.get('released_at')).fromNow()}</p>
+        <p><strong>Average Rating:</strong> {this.props.movie.get('avg_rating')}</p>
+        <p>
+          <strong>IMDB:</strong>
+          <a href={this.props.movie.get('imdb_link')}>
+          {this.props.movie.get('imdb_link')}</a>
+        </p>
+      </div>
     );
   },
 });
