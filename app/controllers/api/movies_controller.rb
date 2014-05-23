feae2446f7325e0dfdfa03117b4ce45d6b74cce4
CJ Avilla <cjavilla@gmail.com>
2014-05-22 21:25:42 -0700

    add all movies back for movies controller

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -2,7 +2,7 @@ class Api::MoviesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @movies = Movie.with_avg_rating.order(released_at: :asc).limit(50)
+    @movies = Movie.with_avg_rating.order(released_at: :asc)
     @ratings = {}
     current_user.ratings.map { |r| @ratings[r.movie_id] = r }
     render :index
