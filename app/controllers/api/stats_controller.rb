e1110e561815bf56e58803abdb24c48818508ab4
CJ Avilla <cjavilla@gmail.com>
2014-05-25 12:02:55 -0700

    adding stats

--- app/controllers/api/stats_controller.rb
+++ app/controllers/api/stats_controller.rb
@@ -3,5 +3,14 @@ module Api
     def index
       render json: current_user.stats
     end
+
+    def show
+      redirect_to 'api/stats' unless current_user.id == 1
+
+      render json: {
+        user_count: User.count,
+        rating_count: Rating.count
+      }
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     resources :movies
     resources :ratings
-    resources :stats, only: [:index]
+    resources :stats, only: [:index, :show]
   end
 end
